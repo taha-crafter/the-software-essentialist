5b0b60b428c7298f4cfafb1a81311fca004cb9bc
Taha <taha@marketer.ly>
2024-10-21 12:47:57 +0200

    Finish setting up and bootstrapping the app

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/bootstrap.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/bootstrap.ts
@@ -1,9 +1,42 @@
-import { PrismaClient } from "@prisma/client";
-
-import Database from "./database";
-import StudentsService from "./services/students";
-import StudetnsController from "./controllers/students";
-
 import Server from "./server";
+import Database from "./database";
+
+import {
+  StudentsService,
+  AssignmentsService,
+  ClassesService,
+} from "./services/";
+import {
+  StudetnsController,
+  ClassesController,
+  AssignmentsController,
+} from "./controllers/";
 
 import { errorHandler } from "./shared/errors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const db = new Database(prisma);
+
+const studentsService = new StudentsService(db);
+const classesService = new ClassesService(db);
+const assignmentsService = new AssignmentsService(db);
+
+const studentsController = new StudetnsController(
+  studentsService,
+  errorHandler
+);
+const classesController = new ClassesController(classesService, errorHandler);
+const assignmentsController = new AssignmentsController(
+  assignmentsService,
+  errorHandler
+);
+
+const server = new Server(
+  studentsController,
+  classesController,
+  assignmentsController
+);
+
+export default server;
---
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/controllers/index.ts
@@ -0,0 +1,5 @@
+import StudetnsController from "./students";
+import ClassesController from "./classes";
+import AssignmentsController from "./assignments";
+
+export { StudetnsController, ClassesController, AssignmentsController };
